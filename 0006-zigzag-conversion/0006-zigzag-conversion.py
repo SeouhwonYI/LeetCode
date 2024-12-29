b1fccf95c6047aaab2f17c4af588cb2d432d61a9
SeouhwonYi <100933409+SeouhwonYI@users.noreply.github.com>
2024-12-29 18:48:24 +0900

    Time: 8 ms (77.87%), Space: 18.1 MB (12.31%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,18 +1,31 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # if numRows == 1:
+        #     return s
+        # rows = {}
+        # for i in range(numRows):
+        #     rows[i] = ""
+        # row = 0
+        # direction = 1
+        # for i in range(len(s)):
+        #     rows[row] += s[i]
+        #     row += direction
+        #     if row == numRows-1 or row == 0:
+        #         direction *= -1
+        # result = ""
+        # for value in rows.values():
+        #     result += value
+        # return result
+
         if numRows == 1:
             return s
-        rows = {}
-        for i in range(numRows):
-            rows[i] = ""
-        row = 0
-        direction = 1
-        for i in range(len(s)):
-            rows[row] += s[i]
-            row += direction
-            if row == numRows-1 or row == 0:
-                direction *= -1
-        result = ""
-        for value in rows.values():
-            result += value
-        return result
+        rows = [''] * min(numRows, len(s))
+        curRow, goingDown = 0, False
+
+        for c in s:
+            rows[curRow] += c
+            if curRow == 0 or curRow == numRows - 1:
+                goingDown = not goingDown
+            curRow += 1 if goingDown else -1
+
+        return ''.join(rows)
