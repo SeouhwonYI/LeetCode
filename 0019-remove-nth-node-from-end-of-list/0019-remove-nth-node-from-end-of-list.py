86d6d7341991d4d303fe2b8795e360698fd1c5fa
SeouhwonYi <100933409+SeouhwonYI@users.noreply.github.com>
2024-12-31 18:24:45 +0900

    Time: 0 ms (100%), Space: 17.7 MB (10.85%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -19,3 +19,17 @@ class Solution:
             nodes[-n-1].next = nodes[-n+1]
         
         return head
+
+        # res = ListNode(0, head)
+        # dummy = res
+
+        # for _ in range(n):
+        #     head = head.next
+        
+        # while head:
+        #     head = head.next
+        #     dummy = dummy.next
+        
+        # dummy.next = dummy.next.next
+
+        # return res.next
