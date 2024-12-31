2edd54e94821bfe4f2e67e9ab16e05cd14ad9d3d
SeouhwonYi <100933409+SeouhwonYI@users.noreply.github.com>
2024-12-31 22:45:00 +0900

    Time: 15 ms (62.58%), Space: 19.9 MB (44.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,17 +1,32 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        # m, n = len(grid), len(grid[0])
+        # itr = m + n -2
+
+        # for k in range(itr):
+        #     # (0, k+1), (1, k), (2, k-1), ... , (k+1, 0)
+        #     for i in range(k+2):
+        #         j = (k+1) - i
+        #         if (i < m and j < n):
+        #             if (i == 0): up = 200 * itr + 1
+        #             else: up = grid[i-1][j]
+        #             if (j == 0): left = 200 * itr + 1
+        #             else: left = grid[i][j-1]
+
+        #             grid[i][j] = min(up, left) + grid[i][j]
+
+        # return grid[m-1][n-1]
+
         m, n = len(grid), len(grid[0])
-        itr = m + n -2
-
-        for k in range(itr):
-            # (0, k+1), (1, k), (2, k-1), ... , (k+1, 0)
-            for i in range(k+2):
-                j = (k+1) - i
-                if (i < m and j < n):
-                    if (i == 0): up = 200 * itr + 1
-                    else: up = grid[i-1][j]
-                    if (j == 0): left = 200 * itr + 1
-                    else: left = grid[i][j-1]
-
-                    grid[i][j] = min(up, left) + grid[i][j]
-        return grid[m-1][n-1]
+        
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
