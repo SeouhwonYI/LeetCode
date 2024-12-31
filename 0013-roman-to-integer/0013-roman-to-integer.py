7050a6793f0529c1a6ff34a0bc1d1f2e3e741e34
SeouhwonYi <100933409+SeouhwonYI@users.noreply.github.com>
2024-12-31 16:19:20 +0900

    Time: 0 ms (100%), Space: 17.6 MB (18.55%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,12 +1,29 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {"I": 1, "V": 5, "X": 10, "L": 50,
-                 "C": 100, "D": 500, "M": 1000}
-        sol = 0
-        for i in range(len(s)):
-            if i < len(s)-1 and roman[s[i]] < roman[s[i+1]]:
-                sol -= roman[s[i]]
-            else:
-                sol += roman[s[i]]
+        # roman = {"I": 1, "V": 5, "X": 10, "L": 50,
+        #          "C": 100, "D": 500, "M": 1000}
+        # sol = 0
+        # for i in range(len(s)):
+        #     if i < len(s)-1 and roman[s[i]] < roman[s[i+1]]:
+        #         sol -= roman[s[i]]
+        #     else:
+        #         sol += roman[s[i]]
 
-        return sol
+        # return sol
+
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
