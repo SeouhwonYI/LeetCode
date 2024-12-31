cb4f34aef07b651f04e9448fe594a4d465c671ce
SeouhwonYi <100933409+SeouhwonYI@users.noreply.github.com>
2024-12-31 21:13:16 +0900

    Time: 14 ms (77.71%), Space: 18.7 MB (12.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,24 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        for i in range(1, len(nums)):
-            nums[i] = max(i+nums[i], nums[i-1])
-        pos = 0
-        while pos < len(nums)-1:
-            if nums[pos] == pos:
-                return False
-            pos = nums[pos]
+        # for i in range(1, len(nums)):
+        #     nums[i] = max(i+nums[i], nums[i-1])
+        # pos = 0
+        # while pos < len(nums)-1:
+        #     if nums[pos] == pos:
+        #         return False
+        #     pos = nums[pos]
         
+        # return True
+
+        gas = 0
+        for n in nums:
+            # can't progress
+            if gas < 0:
+                return False
+            # n: new gas vs gas: current gas
+            elif n > gas:
+                gas = n
+            # can reach 'n' but 
+            gas -= 1
+            
         return True
